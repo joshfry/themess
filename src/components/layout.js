a16d7c3987ac72ee2ce69865111c5550b45e1fea
joshfry <joshdfry@me.com>
2019-03-25 21:51:51 -0700

    page reorg

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
-// import Header from './header';
+import Header from './header';
 import './layout.scss';
 
 const Layout = ({ children }) => (
@@ -25,7 +25,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <React.Fragment>
-        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+        <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
             margin: `0 auto`,
@@ -35,11 +35,11 @@ const Layout = ({ children }) => (
           }}
         >
           <main>{children}</main>
-          {/* <footer>
+          <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer> */}
+          </footer>
         </div>
       </React.Fragment>
     )}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,13 @@
 import React from 'react';
+import SEO from 'components/seo';
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import logo from '../images/the-mess-logo.svg';
+import Splash from 'components/Splash';
 
 const IndexPage = () => (
-  <Layout>
+  <React.Fragment>
     <SEO title="Home" />
-    <h1
-      style={{
-        opacity: `0`,
-        position: `fixed`,
-        top: `0`,
-        left: `0`,
-        height: `0`,
-        margin: `0`,
-      }}
-    >
-      The Mess
-    </h1>
-    <div
-      style={{
-        display: `flex`,
-        flexDirection: `column`,
-        alignItems: `center`,
-        justifyContent: `center`,
-        padding: `5vw`,
-        height: `90vh`,
-      }}
-    >
-      <div
-        style={{
-          width: `100%`,
-          maxWidth: `600px`,
-          margin: `0 auto`,
-        }}
-      >
-        <img src={logo} alt="" />
-      </div>
-      <h2
-        style={{
-          fontSize: `40px`,
-        }}
-      >
-        Coming Soon
-      </h2>
-    </div>
-  </Layout>
+    <Splash />
+  </React.Fragment>
 );
 
 export default IndexPage;
