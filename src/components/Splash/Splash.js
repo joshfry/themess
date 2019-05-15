e895cff87f7a5a53647b688f020a8c47e705c3b3
joshfry <joshdfry@me.com>
2019-05-14 18:17:47 -0700

    added google analytics

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { OutboundLink } from 'gatsby-plugin-google-analytics';
 
 import Logo from 'components/Logo';
 import Icon from 'components/Icon';
@@ -13,19 +14,19 @@ const Splash = () => (
     </div>
     <div className="Splash__social-links">
       <div className="Splash__social-links__item">
-        <a href="https://www.facebook.com/official.the.mess/">
+        <OutboundLink href="https://www.facebook.com/official.the.mess/">
           <Icon name="Facebook" color="#f1c15d" />
-        </a>
+        </OutboundLink>
       </div>
       <div className="Splash__social-links__item">
-        <a href="https://www.instagram.com/themessofficial">
+        <OutboundLink href="https://www.instagram.com/themessofficial">
           <Icon name="Instagram" color="#f1c15d" />
-        </a>
+        </OutboundLink>
       </div>
       <div className="Splash__social-links__item">
-        <a href="https://shop.spreadshirt.com/the-mess">
+        <OutboundLink href="https://shop.spreadshirt.com/the-mess">
           <Icon name="Shop" color="#f1c15d" />
-        </a>
+        </OutboundLink>
       </div>
     </div>
   </div>
