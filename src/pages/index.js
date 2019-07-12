b3747514ec5bacb538ea3d7011e6cd1a0cd900eb
joshfry <joshdfry@me.com>
2019-07-12 09:58:01 -0700

    fixed graphql query to allow multiple images on one page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StaticQuery, graphql, Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
 
 import SEO from 'components/SEO';
@@ -7,84 +7,58 @@ import Layout from 'components/Layout';
 import Container from 'components/Container';
 import ContainerInner from 'components/ContainerInner';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Container>
-      <ContainerInner superNarrow>
-        <div>
-          <h2>Worth Fighting For - Out Now</h2>
-          <div className="linkto">
-            <StaticQuery
-              query={graphql`
-                query {
-                  one: file(
-                    relativePath: { eq: "images/worth-fighting-for-cover.jpg" }
-                  ) {
-                    childImageSharp {
-                      fluid(maxWidth: 500) {
-                        base64
-                        tracedSVG
-                        aspectRatio
-                        src
-                        srcSet
-                        srcWebp
-                        srcSetWebp
-                        sizes
-                        originalImg
-                        originalName
-                        presentationWidth
-                        presentationHeight
-                      }
-                    }
-                  }
-                }
-              `}
-              render={data => <Img fluid={data.one.childImageSharp.fluid} />}
-            />
+const IndexPage = pageQuery => {
+  const { realities, worthFightingFor } = pageQuery.data;
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Container>
+        <ContainerInner superNarrow>
+          <div>
+            <h2>Worth Fighting For - Out Now</h2>
+            <Img fluid={worthFightingFor.childImageSharp.fluid} />
             <br />
-            <Link to="/music/worth-fighting-for">
-              Stream &ldquo;Worth&nbsp;Fighting&nbsp;For&rdquo;
-            </Link>
+            <div className="linkto">
+              <Link to="/music/worth-fighting-for">
+                Stream &ldquo;Worth&nbsp;Fighting&nbsp;For&rdquo;
+              </Link>
+            </div>
           </div>
-        </div>
-        <div>
-          <h2>Realities</h2>
-          <div className="linkto">
-            {/* <StaticQuery
-              query={graphql`
-                query {
-                  two: file(
-                    relativePath: { eq: "images/realities-cover.jpg" }
-                  ) {
-                    childImageSharp {
-                      fluid(maxWidth: 500) {
-                        base64
-                        tracedSVG
-                        aspectRatio
-                        src
-                        srcSet
-                        srcWebp
-                        srcSetWebp
-                        sizes
-                        originalImg
-                        originalName
-                        presentationWidth
-                        presentationHeight
-                      }
-                    }
-                  }
-                }
-              `}
-              render={data => <Img fluid={data.two.childImageSharp.fluid} />}
-            />
-            <br /> */}
-            <Link to="/music/realities">Stream &ldquo;Realities&rdquo;</Link>
+          <div>
+            <h2>Realities</h2>
+            <Img fluid={realities.childImageSharp.fluid} />
+            <br />
+            <div className="linkto">
+              <Link to="/music/realities">Stream &ldquo;Realities&rdquo;</Link>
+            </div>
           </div>
-        </div>
-      </ContainerInner>
-    </Container>
-  </Layout>
-);
+        </ContainerInner>
+      </Container>
+    </Layout>
+  );
+};
+
+export const squareImage = graphql`
+  fragment squareImage on File {
+    childImageSharp {
+      fluid(maxWidth: 500, maxHeight: 500) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+`;
+
+export const pageQuery = graphql`
+  query {
+    realities: file(relativePath: { eq: "images/realities-cover.jpg" }) {
+      ...squareImage
+    }
+    worthFightingFor: file(
+      relativePath: { eq: "images/worth-fighting-for-cover.jpg" }
+    ) {
+      ...squareImage
+    }
+  }
+`;
 
 export default IndexPage;
